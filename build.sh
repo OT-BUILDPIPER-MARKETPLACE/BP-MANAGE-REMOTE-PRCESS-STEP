9f83a4fa520eefa74e6b0b6c51dd97b63e802d68
Sandeep <sandeep@opstree.com>
2022-11-27 15:43:07 +0530

    added command to perform operation

--- build.sh
+++ build.sh
@@ -31,4 +31,6 @@ logInfoMessage "Action to be performed: ${ACTION}"
 logInfoMessage "Process name on which actions is to be performed: ${PROCESS}"
 logInfoMessage "Alias of host where action is to be performed: ${HOST}"
 
+ssh ${HOST} "sudo systemctl $ACTION ${PROCESS}"
+
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
